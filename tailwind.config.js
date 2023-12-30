fd6cdd5f3b8268640e758566a8cc09e7de7d07be
Florent Gilet <flo.gilet@hotmail.fr>
2023-12-30 11:37:48 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    colors: {
+      'color1': '#151515',
+      'color2': '#4EE1A0',
+      'color3': '#242424',
+      'color4': '#D9D9D9'
+    },
+
+    fontFamily: {
+      'primary': "SpaceGrotesk"
+    },
+
+    fontSize: {
+      'typo-xl': '88px',
+      'typo-l': '48px',
+      'typo-m': '24px',
+      'typo': '18px'
+    },
+    spacing: {
+      
+    }
+  }
+  // ...
+}
+
